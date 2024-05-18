7585487d1cc883fd9bf9d90251fcbcccd8514fec
kkang0 <whye8510@hanyang.ac.kr>
2024-05-18 15:08:58 +0900

    [Feat/#4]: 게시글 API 기능 구현

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,14 +1,123 @@
+const { StatusCodes } = require("http-status-codes");
+const postService = require("../services/postService");
+const { verifyAccessToken } = require("../utils/verifyToken");
+const valid = require("../utils/validation");
+
 // 게시글 작성
-const writePost = (req, res) => {};
+const writePost = [
+  valid.tokenValidation(),
+  valid.validationCheck,
+  async (req, res) => {
+    const { categoryId, title, content, links } = req.body;
+
+    try {
+      const token = req.headers["authorization"].split(" ")[1];
+      const verifyResult = verifyAccessToken(token);
+
+      if (verifyResult) {
+        const writerId = verifyResult.userId;
+        const result = await postService.writePost(
+          writerId,
+          categoryId,
+          title,
+          content,
+          links
+        );
+
+        res.status(StatusCodes.CREATED).json(result);
+      }
+    } catch (err) {
+      res.status(err.StatusCodes || 400).json({
+        isSuccess: false,
+        message: err.message,
+      });
+    }
+  },
+];
 
 // 게시글 조회
-const getPosts = (req, res) => {};
+const getPosts = [
+  valid.tokenValidation(),
+  valid.validationCheck,
+  async (req, res) => {
+    try {
+      const token = req.headers["authorization"].split(" ")[1];
+      const verifyResult = verifyAccessToken(token);
+
+      if (verifyResult) {
+        const { userId } = verifyResult;
+        const { limit, page, myPage, search, categoryId } = req.query;
+
+        const result = await postService.getPosts(
+          userId,
+          parseInt(limit),
+          parseInt(page),
+          myPage === "true",
+          search,
+          categoryId
+        );
+
+        res.status(StatusCodes.OK).json(result);
+      }
+    } catch (err) {
+      res.status(err.statusCode || 400).json({
+        isSuccess: false,
+        message: err.message,
+      });
+    }
+  },
+];
 
 // 게시글 상세 조회
-const getPostDetail = (req, res) => {};
+const getPostDetail = [
+  valid.tokenValidation(),
+  valid.validationCheck,
+  async (req, res) => {
+    const { postId } = req.params;
+
+    try {
+      const token = req.headers["authorization"].split(" ")[1];
+      const verifyResult = verifyAccessToken(token);
+
+      if (verifyResult) {
+        const result = await postService.getPostDetail(postId);
+        res.status(StatusCodes.OK).json(result);
+      }
+    } catch (err) {
+      res.status(err.statusCode || 400).json({
+        isSuccess: false,
+        message: err.message,
+      });
+    }
+  },
+];
 
 // 게시글 삭제
-const deletePost = (req, res) => {};
+const deletePost = [
+  valid.tokenValidation(),
+  valid.validationCheck,
+  async (req, res) => {
+    const { postId } = req.params;
+
+    try {
+      const token = req.headers["authorization"].split(" ")[1];
+      const verifyResult = verifyAccessToken(token);
+
+      if (verifyResult) {
+        const result = await postService.deletePost(
+          verifyResult.userId,
+          postId
+        );
+        res.status(StatusCodes.OK).json(result);
+      }
+    } catch (err) {
+      res.status(err.statusCodes || 400).json({
+        isSuccess: false,
+        message: err.message,
+      });
+    }
+  },
+];
 
 module.exports = {
   writePost,
