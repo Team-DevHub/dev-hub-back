f302bb085729f3b295ed8d6df8bd0c237ce23963
JIMIN1020 <jimins4920@gmail.com>
2024-05-30 13:33:47 +0900

    [Feat/#12]: cors 설정

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
+const cors = require("cors");
 const express = require("express");
 const app = express();
 
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
+
 app.listen(process.env.PORT, () => {
   console.log(`Server listening on ${process.env.PORT}...`);
 });
