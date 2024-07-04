75499d6f77ee40bba36fbe44c4dbd5dcaf054f6b
JIMIN1020 <jimins4920@gmail.com>
2024-07-04 18:38:58 +0900

    [Feat/#45]: 유효성 검사 추가

--- controllers/scrapController.js
+++ controllers/scrapController.js
@@ -1,5 +1,6 @@
 const { StatusCodes } = require("http-status-codes");
 const scrapService = require("../services/scrapService");
+const valid = require("../utils/validation");
 
 const getScrapList = async (req, res) => {
   try {
@@ -13,31 +14,39 @@ const getScrapList = async (req, res) => {
   }
 };
 
-const scrap = async (req, res) => {
-  try {
-    const { postId } = req.params;
-    const result = await scrapService.scrap(req.userId, postId);
-    res.status(StatusCodes.OK).json(result);
-  } catch (err) {
-    res.status(err.statusCode || 500).json({
-      isSuccess: false,
-      message: err.message,
-    });
-  }
-};
+const scrap = [
+  valid.postIdValidation(),
+  valid.validationCheck,
+  async (req, res) => {
+    try {
+      const { postId } = req.params;
+      const result = await scrapService.scrap(req.userId, postId);
+      res.status(StatusCodes.OK).json(result);
+    } catch (err) {
+      res.status(err.statusCode || 500).json({
+        isSuccess: false,
+        message: err.message,
+      });
+    }
+  },
+];
 
-const deleteScrap = async (req, res) => {
-  try {
-    const { postId } = req.params;
-    const result = await scrapService.deleteScrap(req.userId, postId);
-    res.status(StatusCodes.OK).json(result);
-  } catch (err) {
-    res.status(err.statusCode || 500).json({
-      isSuccess: false,
-      message: err.message,
-    });
-  }
-};
+const deleteScrap = [
+  valid.postIdValidation(),
+  valid.validationCheck,
+  async (req, res) => {
+    try {
+      const { postId } = req.params;
+      const result = await scrapService.deleteScrap(req.userId, postId);
+      res.status(StatusCodes.OK).json(result);
+    } catch (err) {
+      res.status(err.statusCode || 500).json({
+        isSuccess: false,
+        message: err.message,
+      });
+    }
+  },
+];
 
 module.exports = {
   getScrapList,
